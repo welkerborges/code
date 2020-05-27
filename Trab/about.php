7fb9ac6352046218689293a78f209dbe62d84fc8
welkerborges <60852538+welkerborges@users.noreply.github.com>
2020-05-27 09:42:10 -0300

    ´TESTE
    teste

---
+++ Trab/about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/about.css"/>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<nav id="nav">
+	<?php 	
+	require_once('header.html');?>
+	</nav>
+<!------------------------------------CORPO------------------#home2img----------------------- -->	
+	
+	<div id="corpo"><br><br>
+		<img id="home2img" src="img/index1.png" alt="sobre nós"><br>
+		<div id="home1">
+			<div id="sobrenos">
+					<h1><b>Sobre nós</b></h1>
+						<p>
+							O projeto visa orientar o usuario atrvés
+							de uma intefaca facilitadora, disponibilizando informações 
+							como IMC, calculos de calorias de alimentos,dicas de saude e topicos 
+							de conscientização ao meio ambiente
+						</p>
+			</div>
+		</div>
+
+		<div id="home2">
+			
+				<img id="indeximg" src="img/index1.png" alt="sobre nós">
+
+		</div>
+	</div>
+
+<!---------------------------------------------------------------------------------- -->	
+	<?php	
+	require_once('footer.html');?>
+
+</body>
+</html>
---
+++ Trab/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="css/index.css"/>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<nav id="nav">
+	<?php 	
+	require_once('header.html');?>
+	</nav>
+<!------------------------------------CORPO------------------#home2img----------------------- -->	
+	<div id="index">
+	<div id="corpo"><br><br>
+		<img id="home2img" src="img/index1.png" alt="sobre nós"><br>
+		<div id="index1">
+			<div id="sobrenos">
+				<h1><b>Seja bem vindo</b></h1>
+					<p>
+							O projeto visa orientar o usuario atrvés
+							de uma intefaca facilitadora, disponibilizando informações 
+							como IMC, calculos de calorias de alimentos,dicas de saude e topicos 
+							de conscientização ao meio ambiente
+					</p>
+			</div>
+		</div>
+		<div id="index2">	
+			<img id="indeximg" src="img/index1.png" alt="Home">
+		</div>
+	</div>
+<!--PARTE INFERIOR --><br><br><br>
+<br>
+	<div class="corpo1">
+		<div class="index0">		
+				<img id="indeximg0" src="img/corona.jpg" alt="Home">
+		</div>
+
+		<div class="indexinfo">	
+				<div>
+					<h1><b>Coronavirus, Saiba como se proteger</b></h1>
+					<p>
+							O projeto visa orientar o usuario atrvés
+							de uma intefaca facilitadora, disponibilizando informações 
+							como IMC, calculos de calorias de alimentos,dicas de saude e topicos 
+							de conscientização ao meio ambiente
+					</p>
+				</div>
+		</div>
+	</div>
+
+	<div class="corpo1">
+		<div class="index0">		
+				<img id="indeximg0" src="img/index0.png" alt="Home">
+		</div>
+
+		<div class="indexinfo">		
+				<div>
+					<h1><b>Alimentação saudável</b></h1>
+					<p>
+							O projeto visa orientar o usuario atrvés
+							de uma intefaca facilitadora, disponibilizando informações 
+							como IMC, calculos de calorias de alimentos,dicas de saude e topicos 
+							de conscientização ao meio ambiente
+					</p>
+
+				</div>
+		</div>
+
+
+	</div>
+	</div>
+<!---------------------------------------------------------------------------------- -->	
+	<?php	
+	require_once('footer.html');?>
+
+</body>
+</html>
